cc4b1235279da3c97a4787cd1974812aae72a350
Daniel Collazzo <collazzo.d@gmail.com>
2020-05-15 20:10:45 +0200

    Adjust screen resolution

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 int main() {
   constexpr std::size_t kFramesPerSecond{60};
   constexpr std::size_t kMsPerFrame{1000 / kFramesPerSecond};
-  constexpr std::size_t kScreenWidth{640};
-  constexpr std::size_t kScreenHeight{640};
+  constexpr std::size_t kScreenWidth{1280};
+  constexpr std::size_t kScreenHeight{720};
 
   Renderer renderer(kScreenWidth, kScreenHeight);
   char c;
