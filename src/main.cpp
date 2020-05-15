bddb7de948355a2b32768d6ada09d82e8c630e2f
Daniel Collazzo <collazzo.d@gmail.com>
2020-05-15 20:21:25 +0200

    Add class controller

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "renderer.h"
+#include "controller.h"
 
 int main() {
   constexpr std::size_t kFramesPerSecond{60};
@@ -7,6 +8,7 @@ int main() {
   constexpr std::size_t kScreenWidth{1280};
   constexpr std::size_t kScreenHeight{720};
 
+  Controller controller;
   Renderer renderer(kScreenWidth, kScreenHeight);
   char c;
   std::cin >> c; 
