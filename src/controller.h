a7f381f23315b31264f326709f0dae1c1693b6a0
Daniel Collazzo <collazzo.d@gmail.com>
2020-05-24 22:28:12 +0200

    Add comments

--- src/controller.h
+++ src/controller.h
@@ -3,6 +3,9 @@
 
 #include "player.h"
 
+/*
+ * Class Controller handles all the input from keyboard and update the Player's Spaceship accordingly
+*/
 class Controller {
 public:
   void HandleInput(bool &running, Player *player) const;
